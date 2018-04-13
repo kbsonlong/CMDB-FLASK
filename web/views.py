57713db8cf7ce4bc6f0b9c7f2cc261ba952b98de
kbsonlong <kbsonlong@gmail.com>
2018-04-13 18:06:10 +0800

    ADD test-zapi.py

--- web/views.py
+++ web/views.py
@@ -63,7 +63,6 @@ def zabbix(htmlname):
     r = requests.get(url, headers=headers)
     result = json.loads(r.content)
     if int(validate_result['code']) == 0:
-
         return render_template('WEB/'+htmlname+'.html',info=session,username=session['username'])
     else:
         return render_template('WEB/'+htmlname+'.html',errmsg=validate_result['errmsg'])
