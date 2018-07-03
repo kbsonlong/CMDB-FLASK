f028e137c9ffba8e77a3d7bd62ddb8871a3dca4c
kbsonlong <13610018065@139.com>
2018-07-03 21:21:35 +0800

    Update config.py

--- config.py
+++ config.py
@@ -15,7 +15,7 @@ class DevConfig(Config):
     SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:kbsonlong@btc.frp.along.party:8080/cmdb-flask'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     ##解决the session is unavailable because no secret key was set.错误
-    SECRET_KEY='kbsonlong'
+    SECRET_KEY='kbsonlong2'
     WEB_LOGFILE='web.log'
     API_LOGFILE='api.log'
     API_HOST='127.0.0.1:5001'
